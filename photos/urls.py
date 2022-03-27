5a3702501bcc6a8d2258b8ec11c19cd189ea4559
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-03-28 00:06:20 +0300

    able to sort images into categories

--- photos/urls.py
+++ photos/urls.py
@@ -1,14 +1,24 @@
+from os import name
 from django.urls import include, path
 from .views import index,image_location, search_results
 from django.conf import settings
 from django.conf.urls.static import static
+from .import views
+
+# urlpatterns = [
+#     path('',views.index, index,name='index'),
+#     path(''), views.search_results,name='search'),
+#     path('location/(?P<location>\w+)/', views.image_location, name='location'),
+
+# ]
+
 
 urlpatterns = [
-    path('',index,name='index'),
+    path('', views.index, name='index'),
+    path('search/', views.search_results, name='search'),
+    path('location/(?P<location>\w+)/', views.image_location, name='location'),
 
 ]
-
-
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
