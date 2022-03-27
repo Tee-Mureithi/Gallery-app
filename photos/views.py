f509e04d73b049f5c09b5f4c954e05e0bf14b06a
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-03-27 10:55:22 +0300

    changes the base.html

--- photos/views.py
+++ photos/views.py
@@ -9,7 +9,7 @@ def index(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     print(locations)
-    return render(request, 'index.html', {'images': images[::-1], 'locations': locations})
+    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
 
 
 def image_location(request, location):
