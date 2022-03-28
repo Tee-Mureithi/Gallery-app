1a691004ee96d7aaff07110df150b73b04b92eb3
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-03-28 18:34:23 +0300

    adds env

--- gallery/settings.py
+++ gallery/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*d(d+xcza4h5ncq-)f6$gn#+g!o3vxr_umnsy4a=py(5m+%=s'
+# SECRET_KEY = 'django-insecure-0*d(d+xcza4h5ncq-)f6$gn#+g!o3vxr_umnsy4a=py(5m+%=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
